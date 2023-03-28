3a4da14a64b5d89818b205b739f92cd09c1b450e
Roman <romaneremeev7@mail.ru>
2023-03-28 16:57:17 +0300

    Final commit

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,30 @@
 public class Book {
+    private String bookName;
+    private String toString;
+    private int publishingYear;
+
+
+    public Book(String bookName, Author author, int publishingYear) {
+        this.bookName=bookName;
+        this.toString=toString;
+        this.publishingYear=publishingYear;
+
+    }
+    public String getBookName() {
+        return this.bookName;
+    }
+    public  String getString() {
+        return this.toString;
+    }
+    public int getPublishingYear() {
+        return this.publishingYear;
+    }
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] authorName = {"Л.Н.Толстой", "Ф.М.Достоевский"};
+        String[] bookName = {"Война и Мир", "Преступление и наказание"};
+        int[] publishingYear = {1867, 1866};
+        for (int i = 0; i < authorName.length; i++) {
+            System.out.println("Имя автора - " + authorName[i] + " Имя книги " + bookName[i] + " Год публикации " + publishingYear[i]);
+        }
+        Author lev = new Author("Лев", "Толстой");
+        System.out.println("Лев");
+        Author tolstoy = new Author("Лев", "Толстой");
+        System.out.println("Толстой");
+        Book warAndPeace = new Book("Война и мир", lev, 1867);
+        System.out.println("warAndPeace = " + warAndPeace.getBookName());
+        Book authorBook = new Book("Война и мир", lev, 1867);
+        System.out.println("authorBook = " + authorBook.getString());
+        Book yearBook = new Book("Война и мир", lev, 1867);
+        System.out.println("yearBook = " + yearBook.getPublishingYear());
+
+        Author fedor = new Author("Фёдор", "Достоевский");
+        System.out.println("Фёдор");
+        Author dostoevsky = new Author("Фёдор", "Достоевский");
+        System.out.println("Достоевский");
+        Book crimeAndPunishment = new Book("Преступление и наказание", fedor, 1866);
+        System.out.println("crimeAndPunishment = " + crimeAndPunishment.getBookName());
+        Book authorSecondBook = new Book("Преступление и наказание", fedor, 1866);
+        System.out.println("authorSecondBook = " + authorSecondBook.getString() + authorSecondBook.getString());
+        Book yearSecondBook = new Book("Преступление и наказание", fedor, 1866);
+        System.out.println("yearSecondBook = " + yearSecondBook.getPublishingYear());
+
+        String yearBornFirtsBook = "1866";
+        System.out.println(yearBornFirtsBook.hashCode());
+        String yearBornSecondBook = "1867";
+        System.out.println(yearBornSecondBook.hashCode());
+        if (yearBornFirtsBook.equals(yearBornSecondBook)) {
+            System.out.println("Книги выпускались в один год");
+        } else {
+            System.out.println("Книги выпускались в разные года");
+        }
+
+
     }
 }
+
+
+
+
+
+
+
